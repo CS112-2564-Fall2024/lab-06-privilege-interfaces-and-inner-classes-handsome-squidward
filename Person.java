9b1768c8296096d0da23d78dc2d504a95194a5d3
Lenic3 <lenicems06@gmail.com>
2024-10-07 21:48:18 +0000

    pt 1 complete some part 2 in progress

--- Person.java
+++ Person.java
@@ -1,6 +1,25 @@
-public class Person
+public class Person implements Comparable<Person>
 {
 	/***** TODO: (Part 2) create helper inner class for Identity*****/
+	public class Identity {
+		String pronouns;
+		String background;
+		Identity story;
+
+		public Identity(String pronouns, String background) {
+			this.pronouns = pronouns;
+			this.background = background;
+		}
+
+		public Identity() {
+
+		}
+		
+		public String toString() {
+			return this.pronouns + this.background;
+		}
+
+	}
 
 	// CONSTANT VARIABLES
 	public static final String DEFAULT_NAME = "Jamie Doe";
@@ -72,7 +91,7 @@ public class Person
 	public boolean equals(Object other) 
 	{
 		if(other == null || (!(other instanceof Person))) {
-		      return false;
+			return false;
 		}
 		
 		Person otherPerson = (Person) other;
@@ -81,5 +100,15 @@ public class Person
 	}
 
 	// INTERFACE METHODS
-	/***** TODO: (Part 1) override compareTo method to implement Comparable interface*****/
+	@Override
+	public int compareTo(Person other) {
+		if (this.privilege > other.privilege) {
+			return 1;
+		} else if (this.privilege == other.privilege) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
 }
